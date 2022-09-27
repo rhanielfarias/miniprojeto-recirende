2f0a02e31b0813cb22fda53023830fbdab86cd8d
rhanielfarias <rhanielfarias@gmail.com>
2022-09-27 10:37:39 -0300

    Criando spring boot

---
+++ src/main/java/com/catalisa/miniprojetorecirende/MiniprojetoRecirendeApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.miniprojetorecirende;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojetoRecirendeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojetoRecirendeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalisa/miniprojetorecirende/MiniprojetoRecirendeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.miniprojetorecirende;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojetoRecirendeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
